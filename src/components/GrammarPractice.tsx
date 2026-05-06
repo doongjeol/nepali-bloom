664af90a3c06ecc78ae4c0b74be583c347642b30
SPkimdain <dainkim@fasoo.com>
2026-05-06 12:58:14 +0900

    문법 문제 객관식 수정

--- src/components/GrammarPractice.tsx
+++ src/components/GrammarPractice.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
